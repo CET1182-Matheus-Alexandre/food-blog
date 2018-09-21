5991f17b83e2ee5795326800564d2b891e502d37
Alexandre Pedrecal <pedrecalc@gmail.com>
2018-09-21 11:01:23 -0300

    Card component done, scss card fixed

---
+++ src/components/Card.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+  render() {
+    const {
+      lastPost: {
+        postLink,
+        imageLink,
+        imageDescription,
+        author,
+        authorLink,
+        dateTime,
+        postTitle
+      }
+    } = this.props;
+    return (
+      <div>
+        <a href={postLink}>
+          <div className="post">
+            <img src={imageLink} alt={imageDescription} />
+            <div className="info">
+              <div className="author">
+                <address>
+                  <a href={authorLink}>{author}</a>
+                </address>
+                <time dateTime={dateTime}>{dateTime}</time>
+              </div>
+              <div className="post-title">
+                <a href={postLink}>{postTitle}</a>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    );
+  }
+}
