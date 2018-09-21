b91ab18334a0bd41a1b5b50ea78ca135fb51f0cf
Alexandre Pedrecal <pedrecalc@gmail.com>
2018-09-21 11:28:17 -0300

    TextSection Component: added title prop

--- src/components/TextSection.js
+++ src/components/TextSection.js
@@ -4,13 +4,17 @@ export default class TextSection extends Component {
   render() {
     const {
       image: { imageLink, name, right },
+      title,
       children
     } = this.props;
     return (
       <div>
         <section className="welcome">
           {!right && <img src={imageLink} alt={name} />}
-          <div className="about">{children}</div>
+          <div className="about">
+            <h2>{title}</h2>
+            <div>{children}</div>
+          </div>
           {right && <img src={imageLink} alt={name} />}
         </section>
       </div>
