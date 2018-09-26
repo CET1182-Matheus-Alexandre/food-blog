66caf78ec840ce9dc0b55bb6b6c586cc4cded0d7
Matheus Almeida <mat.almeida@live.com>
2018-09-26 10:25:32 -0300

    moving comment of usage to a unit test

---
+++ test/Header.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Header from '../src/components/Header';
+
+describe('Header', () => {
+  test('should render', () => {
+    const navItens = [
+      {
+        name: 'Inicio',
+        href: '#',
+        active: true
+      },
+      {
+        name: 'Blog',
+        href: '#'
+      },
+      {
+        name: 'Sobre nós',
+        href: '#'
+      }
+    ];
+
+    const rendered = renderer
+      .create(
+        <Header
+          navItens={navItens}
+          title="The Best Food"
+          subTitle="is the one that you share"
+          photo={{
+            photoLink: 'path/to/image.jpg',
+            credits: {
+              author: {
+                authorLink: 'https://link.to.author.page.com',
+                authorName: 'Author Name'
+              },
+              stock: {
+                stockLink: 'https://stock.page.link.com',
+                stockName: 'Image Stock Name'
+              }
+            }
+          }}
+        />
+      )
+      .toJSON();
+    expect(rendered).toBeTruthy();
+  });
+});
