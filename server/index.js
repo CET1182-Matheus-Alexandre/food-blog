e4d69dafd7fba91b2a8be662c88d105b2ab21f17
Matheus Almeida <mat.almeida@live.com>
2018-09-26 23:28:35 -0300

    adding compression to served files

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const expressStaticGzip = require('express-static-gzip');
+const path = require('path');
+
+const app = express();
+
+app.use(morgan('common'));
+
+app.use('/', expressStaticGzip(path.join(__dirname, './../dist/')));
+
+app.get('*.js', (req, res, next) => {
+  req.url = `${req.url}.gz`;
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
+app.use(express.static('src'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Serving in port ${port}`);
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,21 @@
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const CompressionPlugin = require('compression-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new CompressionPlugin({
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240
     })
   ]
 });
