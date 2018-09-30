f39111c90b28e9af6332faf2d502fec47efcd337
Matheus Almeida <mat.almeida@live.com>
2018-09-30 18:48:05 -0300

    adding Home page as main page.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
-      <h1>Welcome to my Boilerplate</h1>
+      <Home />
     </div>
   );
 }
