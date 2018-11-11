757228ae64f4a82ce4ed40b321d297c36287182a
Matheus Almeida <mat.almeida@live.com>
2018-11-10 23:15:00 -0300

    feat: protect admin page from non auth users

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,35 +1,133 @@
 import React, { Component } from 'react';
+import { Redirect } from '@reach/router';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 export default class Admin extends Component {
   state = {
-    posts: []
+    signed: !!firebase.auth().currentUser,
+    currentUser: null,
+    posts: [],
+    title: '',
+    imageUrl: '',
+    imageAuthor: '',
+    html: ''
   };
 
-  componentWillMount = () => {
-    const db = firebase.firestore();
-    const settings = { timestampsInSnapshots: true };
-    db.settings(settings);
+  componentDidMount = () => {
+    if (this.state.signed) {
+      this.db = firebase.firestore();
+      const settings = { timestampsInSnapshots: true };
+      this.db.settings(settings);
 
-    db.collection('posts')
-      .get()
-      .then((querySnapshot) => {
-        const newPosts = [];
-        querySnapshot.forEach((doc) => {
-          newPosts.push(doc.data());
-          console.log(`${doc.id} => ${doc.data()}`);
+      this.db
+        .collection('posts')
+        .get()
+        .then((querySnapshot) => {
+          const newPosts = [];
+          querySnapshot.forEach((doc) => {
+            newPosts.push(doc.data());
+            console.log(`${doc.id} => ${doc.data()}`);
+          });
+          this.setState({
+            posts: newPosts,
+            currentUser: firebase.auth().currentUser
+          });
         });
-        this.setState({ posts: newPosts });
+    }
+  };
+
+  titleHandler = ({ target: { value } }) => {
+    this.setState({ title: value });
+  };
+  imageUrlHandler = ({ target: { value } }) => {
+    this.setState({ imageUrl: value });
+  };
+  imageAuthorHandler = ({ target: { value } }) => {
+    this.setState({ imageAuthor: value });
+  };
+  htmlHandler = ({ target: { value } }) => {
+    this.setState({ html: value });
+  };
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    const { title, imageUrl, imageAuthor, html, currentUser } = this.state;
+
+    this.db.collection('posts').add({
+      author: currentUser.uid,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      title,
+      html,
+      image: {
+        imageUrl,
+        imageAuthor
+      }
+    });
+  };
+
+  logoutHandler = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        this.setState({ signed: false });
       });
   };
 
   render() {
-    return (
-      <div>
-        <h1>oi</h1>
-        {JSON.stringify(this.state.posts)}
-      </div>
-    );
+    const { posts, signed } = this.state;
+
+    if (signed) {
+      if (posts !== []) {
+        return (
+          <div>
+            <h1>oi</h1>
+            <button onClick={this.logoutHandler}>Logout</button>
+            <form className="new-post-form">
+              <label className="form-input" htmlFor="title">
+                Titulo:
+                <input type="text" name="title" onChange={this.titleHandler} />
+              </label>
+              <label className="form-input" htmlFor="imageUrl">
+                Url da Imagem:
+                <input
+                  type="text"
+                  name="imageUrl"
+                  onChange={this.imageUrlHandler}
+                />
+              </label>
+              <label className="form-input" htmlFor="imageAuthor">
+                Autor da Imagem:
+                <input
+                  type="text"
+                  name="imageAuthor"
+                  onChange={this.imageAuthorHandler}
+                />
+              </label>
+              <label className="form-input" htmlFor="html">
+                Post em Html:
+                <input
+                  type="textarea"
+                  name="html"
+                  onChange={this.htmlHandler}
+                />
+              </label>
+              <input
+                className="form-submit"
+                type="submit"
+                value="Entrar"
+                onClick={this.submitHandler}
+              />
+            </form>
+            {JSON.stringify(posts)}
+          </div>
+        );
+      }
+      return <h1>Carregando</h1>;
+    }
+
+    return <Redirect noThrow to="/" />;
   }
 }
